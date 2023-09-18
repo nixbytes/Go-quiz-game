f79478849b9ea0ea89b1ceb49a4b3ff32d151124
nixbytes <real8686@gmail.com>
2023-09-17 23:07:49 -0400

    add flag and open csv

--- GoQuiz.go
+++ GoQuiz.go
@@ -1,7 +1,20 @@
 package main
 
-import "fmt"
+import (
+	"flag"
+	"fmt"
+	"os"
+)
 
 func main() {
-	fmt.Println("")
+	// Define a flag to specify the CSV file name.
+	csvFile := flag.String("csv", "proble.csv", "a csv file format of a question")
+	flag.Parse()
+
+	// Open the CSV file.
+	file, err := os.Open(*csvFileName)
+	if err != nil {
+		exit(fmt.Sprintf("Failed to open the CSV file: %s", *csvFileName))
+	}
+	defer file.Close()
 }
